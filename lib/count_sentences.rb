5c45b6651a2a38f2b497ab62b19b654577c4a02b
David Zombar <davidzombar@gmail.com>
2021-09-14 14:35:51 -0600

    commit tree 1a25f0ef4170c7db9ccfba6d600ff8e7f97ca7ef

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,29 @@
 require 'pry'
 
 class String
-
   def sentence?
-    
+    self.end_with?('.')
   end
 
   def question?
-
+    self.end_with?('?')
   end
 
   def exclamation?
-
+    self.end_with?('!')
   end
 
   def count_sentences
-
+    sentences = 0
+    punctuation = %w[. ? !]
+    self
+      .chars
+      .each
+      .with_index do |char, index|
+        if punctuation.include?(char) && !punctuation.include?(self[index + 1])
+          sentences += 1
+        end
+      end
+    sentences
   end
 end
